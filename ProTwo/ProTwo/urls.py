d1e344c21814f41b1817ad2d0bcde9ff138967c9
prasanganath <sandun.wed@gmail.com>
2019-05-24 10:10:29 +0530

    add template folder

--- ProTwo/ProTwo/urls.py
+++ ProTwo/ProTwo/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.conf.urls import url
 from appTwo import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
+    url(r'^help/',include('appTwo.urls')),
     url(r'^admin/', admin.site.urls),
 ]
