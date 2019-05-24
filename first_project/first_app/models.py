f1eaf66b3b17c148f2b36c61f86d4a1fcd37f223
prasanganath <sandun.wed@gmail.com>
2019-05-24 17:13:29 +0530

    modify models

--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -12,19 +12,19 @@ class Topic(models.Model):
 class Webpage(models.Model):
 	topic = models.ForeignKey(Topic)
 	name = models.CharField(max_length=264,unique=True)
-	url = models.URLField(unique=True) 
+	url = models.URLField(unique=True)  
 
 
 	def __str__(self):
 		return self.name
-
+ 
 class AccessRecord(models.Model):
 	name = models.ForeignKey(Webpage)
 	data = models.DateField()
 
 
 	def __str__(self):
-				return str(self.date)		
+		 return str(self.date)		
 						
 
 
