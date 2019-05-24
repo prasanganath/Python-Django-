b2d538f2cc8a430e0d86c550cdaf1d682ca379ec
prasanganath <sandun.wed@gmail.com>
2019-05-24 09:31:32 +0530

    modify

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	my_dict = {'insert_me':"Hello I am from views.py !"}
-	return render(request,'index.html',context=my_dict)
+	my_dict = {'insert_me':"Now i am coming from!"}
+	return render(request,'first_app/index.html',context=my_dict)
